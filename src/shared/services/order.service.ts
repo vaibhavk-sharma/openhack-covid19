4db0b76def6d1d46aa8aa77a140dbdf8aa54f2c2
abhirajpoot <36495395+abhirajpoot@users.noreply.github.com>
2020-05-10 22:27:33 +0530

    Update order.service.ts

--- src/shared/services/order.service.ts
+++ src/shared/services/order.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ApiRoutes } from '../constants';
-import { order } from '../models/order.model';
+import { Order } from '../models/order.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +11,15 @@ export class OrderService {
 
   constructor(private http: HttpClient) { }
 
-    getSupplier(communityId: string): Observable<any> {
+  getSupplier(communityId: string): Observable<any> {
     return this.http.post(ApiRoutes.GetSupplier, { communityId: communityId });
-    }
-
-    getSupplierItems(supplierId: string): Observable<any> {
-        return this.http.post(ApiRoutes.GetSupplierItems, { supplierId: supplierId });
   }
 
-  createOrder(order: order): Observable<any> {
-    return this.http.post(ApiRoutes.CreateOrder, { order: order });
-}
+  getSupplierItems(supplierId: string): Observable<any> {
+    return this.http.post(ApiRoutes.GetSupplierItems, { supplierId: supplierId });
+  }
 
- 
+  createOrder(order: Order): Observable<any> {
+    return this.http.post(ApiRoutes.CreateOrder, { order: order });
+  }
 }
