6bb0c0040b86f3023deb89723675a55424ad57ef
Vaibhav Kumar Sharma <vaibhav_sharma12@infosys.com>
2020-05-11 16:03:34 +0530

    iamauth plug in should be in small case for cloud enablement
    Changes 'iamauth' to small case to enable cloud deployment

--- openhack-Api/db-utility.js
+++ openhack-Api/db-utility.js
@@ -1,7 +1,7 @@
 var Cloudant = require('@cloudant/cloudant');
 const cloudantServiceUrl = 'https://f9cebdcb-7640-4598-adca-dd505ec06ebe-bluemix.cloudantnosqldb.appdomain.cloud/';
 
-var cloudant = Cloudant({url: cloudantServiceUrl,plugins:{iamAuth:{iamApiKey:'ZEOiHcs9UGglu99D5JSH8KTbi0ttfiLK8hHyIxCNusM-'}} });
+var cloudant = Cloudant({url: cloudantServiceUrl,plugins:{iamauth:{iamApiKey:'ZEOiHcs9UGglu99D5JSH8KTbi0ttfiLK8hHyIxCNusM-'}} });
 
 function getDbs(){
 cloudant.db.list((err, body)=>{
